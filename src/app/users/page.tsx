370547d72731380615a0395fce293d15bfd9619b
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-10-12 18:17:17 +0900

    fetch-data

---
+++ src/app/users/page.tsx
@@ -0,0 +1,29 @@
+// npx create-next-app@latest data-demo
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default async function UsersPage() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+  console.log(users);
+
+  return (
+    <>
+      <h1>UsersPage</h1>
+      {users.map((user: User) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.username}</p>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+        </div>
+      ))}
+    </>
+  );
+}
